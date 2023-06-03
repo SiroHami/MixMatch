a1b7ed98270afc03e9a18e71f38cada45d4558ac
nanyeon99 <nanyeon99@gmail.com>
2023-06-04 01:03:09 +0900

    debug

--- MixMatch.py
+++ MixMatch.py
@@ -169,16 +169,16 @@ def train(labeled_trainloader,
     model.train()
     for batch_idx in range(args.train_iteration):
         try:
-            inputs_x, targets_x = labeled_train_iter.next()
+            inputs_x, targets_x = next(labeled_train_iter)
         except:
             labeled_train_iter = iter(labeled_trainloader)
-            inputs_x, targets_x = labeled_train_iter.next()
+            inputs_x, targets_x = next(labeled_train_iter)
 
         try:
-            (inputs_u, inputs_u2), _ = unlabeled_train_iter.next()
+            inputs_u= next(unlabeled_train_iter)
         except:
             unlabeled_train_iter = iter(unlabeled_trainloader)
-            (inputs_u, inputs_u2), _ = unlabeled_train_iter.next()
+            inputs_u= next(unlabeled_train_iter)
         
         data_time.update(time.time() - end)
 
